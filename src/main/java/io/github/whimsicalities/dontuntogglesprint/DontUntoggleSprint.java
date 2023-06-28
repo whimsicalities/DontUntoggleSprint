61700abe342e75c544f7379f8ee183faa3ffbcc7
whimsicalities <whimsicalities.code@gmail.com>
2023-06-28 17:26:04 +0100

    🚧 Sort of working version.
    Sprint isn't permanently toggled on respawn, it turns off when you stop moving.

---
+++ src/main/java/io/github/whimsicalities/dontuntogglesprint/DontUntoggleSprint.java
@@ -0,0 +1,43 @@
+package io.github.whimsicalities.dontuntogglesprint;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.Minecraft;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DontUntoggleSprint implements ModInitializer {
+
+    private static boolean sprintToggledOnLastDeath = false;
+    public static final Logger LOGGER = LoggerFactory.getLogger("ClientModInitialiser");
+
+    public void onInitialize() {
+        ClientTickEvents.START_CLIENT_TICK.register(client ->
+        {
+            if (client.player != null && sprintToggledOnLastDeath && client.player.isAlive() && client.player.input.hasForwardImpulse()) {
+                if (client.player.isSprinting()) {
+                    sprintToggledOnLastDeath = false;
+                    LOGGER.info("Setting sprint back to false!!!!!"); //todo remove this
+                } else {
+                    client.player.setSprinting(true);
+                    LOGGER.info("Setting sprint to true!!!!!"); //todo remove this
+                }
+            }
+//            if (client.player!=null) {
+//                LOGGER.info("Player is sprinting: " + client.player.isSprinting());
+//            }
+        });
+        LOGGER.info("INITIALISING MOD!!!!!"); //todo remove this
+    }
+
+    public static void handleDeath(){
+        Minecraft minecraft = Minecraft.getInstance();
+        sprintToggledOnLastDeath = (minecraft.options.keySprint.isDown() && minecraft.options.toggleSprint().get());
+        LOGGER.info("!!!!!!!!!! Sprint toggle set to "+(minecraft.options.keySprint.isDown() && minecraft.options.toggleSprint().get())); //todo remove this
+    }
+
+    public static void handleRespawn(){
+        Minecraft minecraft = Minecraft.getInstance();
+        minecraft.options.keySprint.setDown(true);
+    }
+}
---
+++ src/main/java/io/github/whimsicalities/dontuntogglesprint/mixin/PlayerDeathMixin.java
@@ -0,0 +1,25 @@
+package io.github.whimsicalities.dontuntogglesprint.mixin;
+
+import com.mojang.authlib.GameProfile;
+import io.github.whimsicalities.dontuntogglesprint.DontUntoggleSprint;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.AbstractClientPlayer;
+import net.minecraft.client.player.LocalPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LocalPlayer.class)
+public abstract class PlayerDeathMixin extends AbstractClientPlayer {
+    public PlayerDeathMixin(ClientLevel clientLevel, GameProfile gameProfile) {
+        super(clientLevel, gameProfile);
+    }
+
+    @Inject(at = @At("TAIL"), method = "hurtTo")
+    protected void hurtTo(float health, CallbackInfo ci) {
+        if (this.getHealth() <= 0) {
+            DontUntoggleSprint.handleDeath();
+        }
+    }
+}
