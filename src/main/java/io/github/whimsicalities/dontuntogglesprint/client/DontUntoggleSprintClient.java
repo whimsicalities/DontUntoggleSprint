a7498cb1fdd8151f1a075f4482a3dfe7685a7ff2
whimsicalities <whimsicalities.code@gmail.com>
2023-07-30 19:36:49 +0100

    🐛 Use the bound sprint key instead of default

--- src/main/java/io/github/whimsicalities/dontuntogglesprint/client/DontUntoggleSprintClient.java
+++ src/main/java/io/github/whimsicalities/dontuntogglesprint/client/DontUntoggleSprintClient.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 
@@ -24,7 +25,7 @@ public class DontUntoggleSprintClient implements ClientModInitializer {
                 if (client.player.isSprinting()) {
                     sprintToggledOnLastDeath = false;
                 } else {
-                    KeyMapping.set(client.options.keySprint.getDefaultKey(), true);
+                    KeyMapping.set(KeyBindingHelper.getBoundKeyOf(client.options.keySprint), true);
                 }
             }
         });
